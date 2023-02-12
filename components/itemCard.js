0c5583ca12cf7b5f4ebff83829f3704a10a56ded
Amir <iskandar.amir@gmail.com>
2023-02-12 21:38:08 +0800

    Design changes

--- components/itemCard.js
+++ components/itemCard.js
@@ -4,17 +4,16 @@ export default function ItemCard(props) {
   const { product, cart, setCart } = props;
   const { id, name, image, options } = product;
   return (
-    <div className="flex flex-col gap-y-2 border-2 border-black px-1 py-3 h-full justify-center">
+    <div className="flex flex-col gap-y-2 border-2 border-[686667] px-1 py-3 h-full justify-center rounded-2xl">
       <img src={`products/${image}`} alt={`Image of ${name}`} />
-      <div className="text-center">{name}</div>
+      <div className="text-center font-bold">{name}</div>
 
       {options.map(({ price, desc }) => (
         <Fragment key={desc}>
-          <div className="flex gap-x-1 justify-center">
-            <div>{desc}</div>
-            <div>{price}</div>
-          </div>
           <div>
+            <div className="text-center">{desc}</div>
+            <div className="flex gap-x-1 justify-center"></div>
+            <div className="text-center">{price}</div>
             <div className="flex rounded-full border-black border-2 ">
               <span className="inline-flex items-center px-3 rounded-l-full bg-black text-white">
                 Qty
--- pages/index.js
+++ pages/index.js
@@ -8,65 +8,65 @@ export default function Home() {
       name: "Fresh Milk",
       image: "FRESH-MILK-FRONT-VIEW-300x300.png",
       options: [
-        { desc: "200mL", price: "RM 5" },
-        { desc: "1L", price: "RM 10" },
+        { desc: "200mℓ", price: "RM5" },
+        { desc: "1ℓ", price: "RM10" },
       ],
     },
-    {
-      id: "2",
-      name: "Henry Jones",
-      image: "HENRY-JONES-1L-FRONT-VIEW-300x300.png",
-      options: [
-        { desc: "200mL", price: "RM 5" },
-        { desc: "1L", price: "RM 10" },
-      ],
-    },
-    {
-      id: "3",
-      name: "Low Fat",
-      image: "LOW-FAT-1L-SIDE-VIEW-300x300.png",
-      options: [{ desc: "1L", price: "RM 10" }],
-    },
-    {
-      id: "4",
-      name: "Cafe Latte",
-      image: "UHT-CAFE-LATTE-200ML-SIDE-VIEW-300x300.png",
-      options: [
-        { desc: "200mL", price: "RM 5" },
-        { desc: "1L", price: "RM 10" },
-      ],
-    },
+    // {
+    //   id: "2",
+    //   name: "Henry Jones",
+    //   image: "HENRY-JONES-1L-FRONT-VIEW-300x300.png",
+    //   options: [
+    //     { desc: "200mℓ", price: "RM5" },
+    //     { desc: "1ℓ", price: "RM10" },
+    //   ],
+    // },
+    // {
+    //   id: "3",
+    //   name: "Low Fat",
+    //   image: "LOW-FAT-1L-SIDE-VIEW-300x300.png",
+    //   options: [{ desc: "1ℓ", price: "RM10" }],
+    // },
+    // {
+    //   id: "4",
+    //   name: "Cafe Latte",
+    //   image: "UHT-CAFE-LATTE-200ML-SIDE-VIEW-300x300.png",
+    //   options: [
+    //     { desc: "200mℓ", price: "RM5" },
+    //     { desc: "1ℓ", price: "RM10" },
+    //   ],
+    // },
     {
       id: "5",
       name: "Chocolate",
       image: "UHT-CHOCOLATE-FRONT-VIEW-300x300.png",
       options: [
-        { desc: "200mL", price: "RM 5" },
-        { desc: "1L", price: "RM 10" },
+        { desc: "200mℓ", price: "RM5" },
+        { desc: "1ℓ", price: "RM10" },
       ],
     },
-    {
-      id: "6",
-      name: "Soy Chocolate",
-      image: "UHT-CHOCOLATE-SOY-FRONT-VIEW-300x300.png",
-      options: [
-        { desc: "200mL", price: "RM 5" },
-        { desc: "1L", price: "RM 10" },
-      ],
-    },
+    // {
+    //   id: "6",
+    //   name: "Soy Chocolate",
+    //   image: "UHT-CHOCOLATE-SOY-FRONT-VIEW-300x300.png",
+    //   options: [
+    //     { desc: "200mℓ", price: "RM5" },
+    //     { desc: "1ℓ", price: "RM10" },
+    //   ],
+    // },
     {
       id: "7",
       name: "Kurma",
       image: "UHT-KURMA-FRONT-VIEW-300x300.png",
       options: [
-        { desc: "200mL", price: "RM 5" },
-        { desc: "1L", price: "RM 10" },
+        { desc: "200mℓ", price: "RM5" },
+        { desc: "1ℓ", price: "RM10" },
       ],
     },
   ];
   const [cart, setCart] = useState({});
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2 px-2 gap-x-2">
       <div className="flex flex-col mx-auto max-h-screen">
         <img
           src="page-1.jpg"
@@ -80,7 +80,7 @@ export default function Home() {
         <div className="text-center visible hidden sm:block">
           Select your quantity and click order:
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-2 p-2">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
           {products.map((product) => (
             <ItemCard
               product={product}
@@ -92,7 +92,7 @@ export default function Home() {
         </div>
         <div className="h-10"></div>
         <button
-          className="border-2 border-black rounded-xl px-2 py-1 mx-auto"
+          className="border-2 border-black rounded-xl px-2 py-1 mx-auto w-full"
           onClick={() => {
             let text = "Hello, I would like to order:\n";
             for (let [key, value] of Object.entries(cart)) {
