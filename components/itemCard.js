8edbd5dab23074763f986f58c883f9c84f73124d
Amir <iskandar.amir@gmail.com>
2023-02-12 21:32:08 +0800

    Input button partially working

--- components/itemCard.js
+++ components/itemCard.js
@@ -1,6 +1,11 @@
+import {useRef } from "react"
+
 export default function ItemCard(props) {
   const { product, cart, setCart } = props;
   const { id, name, image, options } = product;
+
+  const qtyRef = useRef(null)
+
   return (
     <div className="flex flex-col border-2 border-[686667] px-1 py-3 h-full justify-center rounded-2xl gap-y-2">
       <img src={`products/${image}`} alt={`Image of ${name}`} />
@@ -12,15 +17,18 @@ export default function ItemCard(props) {
           <div className="flex gap-x-1 justify-center"></div>
           <div className="text-center">RM{price.toFixed(2)}</div>
           <div className="flex rounded-full border-gray-600 border-2 w-2/3 mx-auto">
-            <span className="inline-flex items-center px-3 rounded-l-full bg-gray-600 text-white text-sm">
-              Qty
+            <span className="inline-flex items-center px-3 rounded-l-full bg-gray-600 text-white text-sm" onClick={()=>{
+              qtyRef.current.value=qtyRef.current.value-1
+            }}>
+              -
             </span>
             <input
               type="number"
               min="0"
               max="48"
               defaultValue="0"
-              className="flex-1 block w-full rounded-r-full text-sm border-black px-2 text-center"
+              className="flex-1 block w-full text-sm border-black px-2 text-center"
+              ref ={qtyRef}
               placeholder="0"
               
               onChange={(e) => {
@@ -47,6 +55,11 @@ export default function ItemCard(props) {
                 }
               }}
             />
+            <span className="inline-flex items-center px-3 rounded-r-full bg-gray-600 text-white text-sm"onClick={()=>{
+              qtyRef.current.value=qtyRef.current.value+1
+            }}>
+              +
+            </span>
           </div>
         </div>
       ))}
