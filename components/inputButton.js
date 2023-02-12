0b8191cd3288eb68fccfef01a4c22a2c5f74253a
Amir <iskandar.amir@gmail.com>
2023-02-12 21:32:11 +0800

    Make input button working

---
+++ components/inputButton.js
@@ -0,0 +1,77 @@
+import { useRef } from "react";
+
+export default function InputButton(props) {
+  const { id, name, cart, setCart, price, desc } = props;
+  const qtyRef = useRef(null);
+
+  function changeValue(amount) {
+    const qty = new Number(qtyRef.current.value) + amount;
+    qtyRef.current.value = qty;
+    return qty;
+  }
+
+  function setValue() {
+    let qty = new Number(qtyRef.current.value);
+    qty = qty > 48 ? 48 : qty < 0 ? 0 : qty;
+    qtyRef.current.value = qty;
+    return qty;
+  }
+
+  function updateCart(qty) {
+    const index = `${id}: ${name} - ${desc}`;
+    if (qty != 0) {
+      setCart({
+        ...cart,
+        [index]: {
+          message: `${qty} x ${id}: ${name} - ${desc}`,
+          name: name,
+          desc: desc,
+          qty: qty,
+          price: price,
+        },
+      });
+    } else {
+      const oldCart = { ...cart };
+      delete oldCart[index];
+      setCart(oldCart);
+    }
+  }
+
+  return (
+    <div className="flex rounded-full border-gray-600 border-2 w-2/3 mx-auto">
+      <button
+        className="inline-flex items-center px-3 rounded-l-full bg-gray-600 text-white text-sm"
+        onClick={() => {
+          const qty = changeValue(-1);
+          updateCart(qty);
+        }}
+      >
+        -
+      </button>
+      <input
+        type="number"
+        min="0"
+        max="48"
+        defaultValue="0"
+        className="flex-1 block w-full text-sm border-black px-2 text-center"
+        ref={qtyRef}
+        placeholder="0"
+        onChange={(e) => {
+          //   let qty = new Number(e.target.value);
+          //   qty = qty > 48 ? 48 : qty < 0 ? 0 : qty;
+          const qty = setValue(e);
+          updateCart(qty);
+        }}
+      />
+      <button
+        className="inline-flex items-center px-3 rounded-r-full bg-gray-600 text-white text-sm"
+        onClick={() => {
+          const qty = changeValue(1);
+          updateCart(qty);
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+}
--- components/itemCard.js
+++ components/itemCard.js
@@ -1,10 +1,8 @@
-import {useRef } from "react"
+import InputButton from "./inputButton";
 
 export default function ItemCard(props) {
   const { product, cart, setCart } = props;
   const { id, name, image, options } = product;
-
-  const qtyRef = useRef(null)
 
   return (
     <div className="flex flex-col border-2 border-[686667] px-1 py-3 h-full justify-center rounded-2xl gap-y-2">
@@ -16,51 +14,15 @@ export default function ItemCard(props) {
           <div className="text-center">{desc}</div>
           <div className="flex gap-x-1 justify-center"></div>
           <div className="text-center">RM{price.toFixed(2)}</div>
-          <div className="flex rounded-full border-gray-600 border-2 w-2/3 mx-auto">
-            <span className="inline-flex items-center px-3 rounded-l-full bg-gray-600 text-white text-sm" onClick={()=>{
-              qtyRef.current.value=qtyRef.current.value-1
-            }}>
-              -
-            </span>
-            <input
-              type="number"
-              min="0"
-              max="48"
-              defaultValue="0"
-              className="flex-1 block w-full text-sm border-black px-2 text-center"
-              ref ={qtyRef}
-              placeholder="0"
-              
-              onChange={(e) => {
-                let qty = new Number(e.target.value);
-                qty = qty > 48 ? 48 : (qty < 0 ? 0 : qty);
-                e.target.value = qty
-
-                const index = `${id}: ${name} - ${desc}`
-                if (qty!=0){
-                  setCart({
-                    ...cart,
-                    [index]: {
-                      message: `${qty} x ${id}: ${name} - ${desc}`,
-                      name: name,
-                      desc: desc,
-                      qty: qty,
-                      price: price,
-                    },
-                  });
-                }else{
-                  const oldCart = {...cart};
-                  delete oldCart[index]
-                  setCart(oldCart)
-                }
-              }}
-            />
-            <span className="inline-flex items-center px-3 rounded-r-full bg-gray-600 text-white text-sm"onClick={()=>{
-              qtyRef.current.value=qtyRef.current.value+1
-            }}>
-              +
-            </span>
-          </div>
+          <InputButton
+            id={id}
+            name={name}
+            image={image}
+            cart={cart}
+            setCart={setCart}
+            price={price}
+            desc={desc}
+          />
         </div>
       ))}
     </div>
