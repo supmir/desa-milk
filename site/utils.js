b1ed87a38fe167e36ac2c449d8d629b597406bcf
Amir <iskandar.amir@gmail.com>
2023-02-15 12:36:14 +0800

    Temporary stop round up and round down

--- site/utils.js
+++ site/utils.js
@@ -1,7 +1,7 @@
 export function roundUp(num) {
-  return (Math.ceil(num * 100) / 100).toFixed(2);
+  return num.toFixed(2);
 }
 
 export function roundDown(num) {
-  return (Math.floor(num * 100) / 100).toFixed(2);
+  return num.toFixed(2);
 }
